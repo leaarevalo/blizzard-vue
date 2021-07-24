a20bfb61b442ff38c6cb111ac0f7204517ef9da6
leaarevalo <leandro.arevalo@isibit.travel>
2021-07-24 01:17:16 -0300

    first commit on mac

--- diablo3/src/main.js
+++ diablo3/src/main.js
@@ -7,6 +7,7 @@ import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/main.styl'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
